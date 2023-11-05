806d5a7e1bac2f592634d1f79a8037d499db4416
HexMaster101 <luckygupta9219@gmail.com>
2023-11-05 15:33:02 +0530

    V1

--- public/intro.js
+++ public/intro.js
@@ -1,9 +1,5 @@
 const intro = document.getElementById("intro");
 
-const myTimeout = setTimeout(function () {
-	intro.style.display = "none";
-}, 2000);
-
 const MyTimeout = setTimeout(() => {
 	intro.classList.add("transition");
-}, 1750);
+}, 1500);
