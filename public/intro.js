251d990c59b7a99f46910a95b3b4c6c6e7a0fe0e
HexMaster101 <luckygupta9219@gmail.com>
2023-11-05 23:23:06 +0530

    fixed touch issue

--- public/intro.js
+++ public/intro.js
@@ -3,3 +3,7 @@ const intro = document.getElementById("intro");
 const MyTimeout = setTimeout(() => {
 	intro.classList.add("transition");
 }, 1500);
+
+const MyTimeout2 = setTimeout(() => {
+	intro.style.display = "none";
+}, 1900);
